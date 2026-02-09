9f919eeb52bbcea0db6dcac585536516cb489d1e
Sahil Suthar <sahil26dd@gmail.com>
2026-02-09 23:42:54 +0530

    err: code err in server file

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+console.log('DB +++:> ', DB);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.error('DB connection failed:', err.message);
+    process.exit(1);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
