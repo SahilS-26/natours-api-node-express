c45e6accf7b5d9c4c5a7f4a84a05a515f2b25ae2
Sahil Suthar <sahil26dd@gmail.com>
2026-02-15 12:33:42 +0530

    creating a new simple model

--- server.js
+++ server.js
@@ -28,6 +28,23 @@ mongoose
     process.exit(1);
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
