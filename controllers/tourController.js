6aeb9cbe1d81d9cdf6498990def33ae2599539bb
Sahil Suthar <sahil26dd@gmail.com>
2026-02-16 19:20:42 +0530

    adding dorting functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,19 +5,28 @@ exports.getAllTours = async (req, res) => {
     console.log(req.query);
 
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advance Filtering
-    let queryStr = JSON.stringify(req.query);
+    // 1B) Advance Filtering
+    let queryStr = JSON.stringify(queryObj);
     // Adding '$' for the operators for mongoose.
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    // This will return DB query.
+    let query = Tour.find(JSON.parse(queryStr));
 
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      // query = query.sort('-price -ratingsAverage');
+      query = query.sort('-createdAt');
+    }
     // EXECUTE QUERY
     const tours = await query;
 
