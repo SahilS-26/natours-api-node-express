0ff017d4ea61070787f70ed9074b306d6e39244e
Sahil Suthar <sahil26dd@gmail.com>
2026-02-16 19:38:13 +0530

    limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,7 +16,7 @@ exports.getAllTours = async (req, res) => {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    // This will return DB query.
+    // -- This will return DB query --.
     let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
@@ -27,6 +27,15 @@ exports.getAllTours = async (req, res) => {
       // query = query.sort('-price -ratingsAverage');
       query = query.sort('-createdAt');
     }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
