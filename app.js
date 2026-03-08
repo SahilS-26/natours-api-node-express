f185bca9731005d24f1d2dbd6499799ba5a9f3e0
Sahil Suthar <sahil26dd@gmail.com>
2026-03-08 16:39:18 +0530

    handling unhandled routes

--- app.js
+++ app.js
@@ -23,4 +23,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// For unhandled Routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
