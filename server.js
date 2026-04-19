6f16013ecf42e68939718d310b2dba860130a5f8
Sahil Suthar <sahil26dd@gmail.com>
2026-04-19 15:38:33 +0530

    error outside express: unhandled rejections

--- server.js
+++ server.js
@@ -22,13 +22,22 @@ mongoose
   })
   .then(() => {
     console.log('DB connection successful!');
-  })
-  .catch((err) => {
-    console.error('DB connection failed:', err.message);
-    process.exit(1);
   });
+// .catch((err) => {
+//   console.error('DB connection failed:', err.message);
+//   process.exit(1);
+// });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
