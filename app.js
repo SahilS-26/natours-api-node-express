e6aa8f9b0502bd0db5b16b3b90a6a3b32e048085
Sahil Suthar <sahil26dd@gmail.com>
2026-06-15 23:53:01 +0530

    removed unwanted import

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const { whitelist } = require('validator');
 
 const app = express();
 
