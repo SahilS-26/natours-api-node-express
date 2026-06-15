369a1c97d406e65a414f74b4df8478043c198212
Sahil Suthar <sahil26dd@gmail.com>
2026-06-15 23:02:01 +0530

    rate limit increased to 100

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const rateLimiter = rateLimit({
-  max: 5,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
