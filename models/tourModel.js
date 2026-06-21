6d737240410c7584d29435e1ab5a8c01d59191a7
Sahil Suthar <sahil26dd@gmail.com>
2026-06-21 15:28:05 +0530

    comment

--- models/tourModel.js
+++ models/tourModel.js
@@ -107,7 +107,7 @@ const tourSchema = new mongoose.Schema(
     guides: [
       {
         type: mongoose.Schema.ObjectId,
-        ref: 'User',
+        ref: 'User', // Always match the model name
       },
     ],
   },
