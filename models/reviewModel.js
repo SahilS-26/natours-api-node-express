ff3c39501c7d00db34a193c1ee26a9adee8d8554
Sahil Suthar <sahil26dd@gmail.com>
2026-07-12 23:38:07 +0530

    minor  update

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -36,7 +36,7 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'tour',
-    select: ' name',
+    select: 'name',
   }).populate({
     path: 'user',
     select: 'name photo',
