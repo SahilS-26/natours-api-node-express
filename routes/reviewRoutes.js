fade4f14d3a2de12cf0026fd017d3c3972d91ad0
Sahil Suthar <sahil26dd@gmail.com>
2026-07-13 23:25:59 +0530

    nested routes with express

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+// POST /tour/3723/reviews
+// POST /reviews
+
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
