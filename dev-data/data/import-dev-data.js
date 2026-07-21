e57757a4aade1b638f795450dacfad4513ca09f5
Sahil Suthar <sahil26dd@gmail.com>
2026-07-21 23:08:58 +0530

    Importing review and user data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dns = require('dns');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
-const Tour = require('./../../models/tourModel');
+const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModal');
 
 // Configure DNS to use Google's DNS servers (for connection MongoDB)
 dns.setServers(['8.8.8.8', '8.8.4.4']);
@@ -33,10 +36,20 @@ const tours = JSON.parse(
   // fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
   fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'),
 );
+const users = JSON.parse(
+  // fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+  fs.readFileSync(`${__dirname}/users.json`, 'utf-8'),
+);
+const reviews = JSON.parse(
+  // fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data Sucessfully loaded!');
   } catch (err) {
     console.log(err);
@@ -47,6 +60,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data Sucessfully Deleted!');
   } catch (err) {
     console.log(err);
--- models/userModal.js
+++ models/userModal.js
@@ -58,6 +58,7 @@ const userSchema = new mongoose.Schema(
   },
 );
 
+/*
 userSchema.pre('save', async function (next) {
   // Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
@@ -76,6 +77,7 @@ userSchema.pre('save', function (next) {
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
+*/
 
 userSchema.pre(/^find/, function (next) {
   // this points to the current query
